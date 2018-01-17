9d9a734d20a040d6eda4999b390e4c87590a6e48
Michael <michkna3@gmail.com>
2018-01-17 15:35:16 -0600

    added auth for deleting comments and every action besides index and show for posts

--- bredTH/app/controllers/comments_controller.rb
+++ bredTH/app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "mk", password: "secret",
+  only: :destroy
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)

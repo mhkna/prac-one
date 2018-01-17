3be68d8be7f8bd4dff98c59a83c7fd045f2d98e0
Michael <michkna3@gmail.com>
2018-01-17 15:25:23 -0600

    ability to add and delete comments

--- bredTH/app/models/post.rb
+++ bredTH/app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  # dependent: :destroy deletes commments from db when post is deleted
+  has_many :comments, dependent: :destroy
   validates :text, presence: true,
                    length: { minimum: 5 }
 end
